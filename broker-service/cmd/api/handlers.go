766628e1a9ea1bfba5d3a7f00d03505a9ff74651
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-09-12 06:48:49 -0700

    [Docker] | Broker Service Image

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -1,15 +1,6 @@
 package main
 
-import (
-	"net/http"
-	"encoding/json"
-)
-
-type jsonResponse struct {
-	Error bool `json:"error"`
-	Message string `json:"message"`
-	Data any `json:"data,omitempty"`
-}
+import "net/http"
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request){
 	payload := jsonResponse {
@@ -17,8 +8,9 @@ func (app *Config) Broker(w http.ResponseWriter, r *http.Request){
 		Message: "Hit the Broker",
 	}
 
-	out, _ := json.MarshalIndent(payload,"","\t")
-	w.Header().Set("Content-Type","application/json")
-	w.WriteHeader(http.StatusAccepted)
-	w.Write(out)
+	_ = app.writeJSON(w, http.StatusOK, payload)
+	// out, _ := json.MarshalIndent(payload,"","\t")
+	// w.Header().Set("Content-Type","application/json")
+	// w.WriteHeader(http.StatusAccepted)
+	// w.Write(out)
 }
