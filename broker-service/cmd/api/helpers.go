6b0b1ccd206068b8d679c6f5aa0acc1a962d2470
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-09-12 07:15:17 -0700

    [Broker] | Helper Functions

--- broker-service/cmd/api/helpers.go
+++ broker-service/cmd/api/helpers.go
@@ -13,7 +13,7 @@ type jsonResponse struct {
 
 func (app *Config) readJSON(w http.ResponseWriter, r *http.Request, data any) error {
 	maxBytes := 1048576 // 1 MB data size
-	r.Body = http.maxBytesReader(w, r.Body, int64(maxBytes))
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
 
 	dec := json.NewDecoder(r.Body)
 	err := dec.Decode(data)
@@ -54,7 +54,7 @@ func (app *Config) writeJSON(w http.ResponseWriter, status int, data any, header
 func (app *Config) errorJSON(w http.ResponseWriter, err error, status ...int) error {
 	statusCode := http.StatusBadRequest
 	if len(status) > 0 {
-		status = status[0]
+		statusCode = status[0]
 	}
 	
 	var payload jsonResponse 
