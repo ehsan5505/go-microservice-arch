ca0ff8d1061c169fbb573b6476a0a4ee183c7275
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-09-17 08:41:23 -0700

    [Auth] | Prepare the Docker Container Deployment

--- authentication-service/cmd/api/main.go
+++ authentication-service/cmd/api/main.go
@@ -53,7 +53,7 @@ func main(){
 }
 
 func openDB(dsn string) (*sql.DB,error){
-	db, err := pgx.Connect(context.Background(),os.Getenv("DATABASE_URL"))
+	db, err := sql.Connect(context.Background(),os.Getenv("DATABASE_URL"))
 	// db, err := sql.Open("pgx",dsn)
 	if err !=nil {
 		return nil, err
