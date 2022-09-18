d625a7f972434031d9778ae95f43037ca5283d91
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-09-18 09:36:38 -0700

    [Auth] | Deploy | Debug

--- authentication-service/cmd/api/routes.go
+++ authentication-service/cmd/api/routes.go
@@ -20,7 +20,7 @@ func (app *Config) routes() http.Handler {
 		MaxAge: 300,
 	}))
 
-	mux.Use(moddleware.Heartbeat("/ping"))
+	mux.Use(middleware.Heartbeat("/ping"))
 	mux.Post("/authentication",app.Authenticate)
 	return mux
 
