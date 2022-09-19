a37af2cd01bf9ea6e205b1e08a68ed2461a158dc
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-09-19 07:52:37 -0700

    [Logger] | Debug

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -117,7 +117,7 @@ func (app *Config) authenticate (w http.ResponseWriter,a AuthPayload) {
 		app.errorJSON(w, errors.New("Invalid Credentials!"))
 		return 
 	}else if response.StatusCode != http.StatusAccepted {
-		app.errorJSON(w,errors.New(fmt.Sprintf("Code:%i",iresponse.StatusCode)))
+		app.errorJSON(w,errors.New(fmt.Sprintf("Code:%i",response.StatusCode)))
 		// app.errorJSON(w, errors.New(`Error in the Service Call!`))		
 		return 
 	}
