280eb9f6000c9333d502277ab074d5776941c778
Ehsan Rafeeque <ehsan.rafeeque@gfk.com>
2022-09-20 14:52:03 -0700

    [Mail] | Tempate and SendMail Request

--- mail-service/cmd/api/main.go
+++ mail-service/cmd/api/main.go
@@ -4,16 +4,19 @@ import (
 	"net/http"
 	"log"
 	"fmt"
+	"strconv"
 )
 
 type Config struct {
-
+	Mailer Mail
 }
 
 const webPort = "80"
 
 func main() {
-	app := Config{}
+	app := Config{
+		Mailer: createMail
+	}
 
 	log.Println("Starting the Mail Server On Port ");
 
@@ -27,4 +30,20 @@ func main() {
 		log.Panic(err)
 	}
 
+}
+
+func createMail() Mail {
+	port, _ := strconv(.Atoi(os.Getenv("MAIL_PORT")))
+	m:= Mail {
+		Domain: 		os.Getenv("MAIL_DOMAIN"),
+		Host: 			os.Getenv("MAIL_HOST"),
+		Port: 			port,
+		Username: 	os.Genenv("MAIL_USERNAME"),
+		Password: 	os.Getenv("MAIL_PASSWORD"),
+		Encryption: os.Getenv("MAIL_Encryption"),
+		FromName: 	os.Getenv("MAIL_FROM"),
+		FromAddress: os.Getenv("MAIL_FROM_ADDRESS"),
+	}
+
+	return m
 }
